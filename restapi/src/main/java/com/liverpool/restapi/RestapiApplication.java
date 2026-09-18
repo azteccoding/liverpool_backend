2369183b7e749faf51e53fb5ef5fd81bab47ab2e
aztecdev <contactos@mnaztecdev.com>
2026-09-17 22:14:22 -0600

    fix mongodb bug

--- restapi/src/main/java/com/liverpool/restapi/RestapiApplication.java
+++ restapi/src/main/java/com/liverpool/restapi/RestapiApplication.java
@@ -2,12 +2,13 @@ package com.liverpool.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class RestapiApplication {
 	public static void main(String[] args) {
 		var context = SpringApplication.run(RestapiApplication.class, args);
-		org.springframework.core.env.Environment env = context.getEnvironment();
-		System.out.println(">>> DATABASE ACTUAL: test");
+		MongoTemplate mongoTemplate = context.getBean(MongoTemplate.class);
+		System.out.println(">>> BASE DE DATOS REAL USADA POR MONGOTEMPLATE: " + mongoTemplate.getDb().getName());
 	}
 }
