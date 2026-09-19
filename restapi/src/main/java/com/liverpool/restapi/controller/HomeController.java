931645a8d95da27aa99d2e4921c68415c274fe40
aztecdev <contactos@mnaztecdev.com>
2026-09-19 10:07:08 -0600

    adding home controller description

--- restapi/src/main/java/com/liverpool/restapi/controller/HomeController.java
+++ restapi/src/main/java/com/liverpool/restapi/controller/HomeController.java
@@ -1,4 +1,14 @@
 package com.liverpool.restapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+
+    @GetMapping(value = "/", produces = "text/html")
+    public String home() {
+        return "<h1>Este es el back de Liverpool para la UI de Liverpool</h1>" +
+                "<p>API Liverpool funcionando. Endpoints: /pedidos, /pedido, /modificar/pedido/{id}, /borrar/pedido/{id}, /usuarios, /usuario</p>";
+    }
 }
